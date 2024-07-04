d0ca3338bc277877c8ffe0f0a3f46c1520229312
miros777 <syniavskyi1984@gmail.com>
2024-07-04 17:01:36 +0300

    start lessson_4

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import React, {FC} from 'react';
+import React from 'react';
 import './App.css';
-import MyButtonComponent from "./components/myCustomButtonComponent/MyCustomButtonComponent";
 
 const App = () => {
   return (
       <>
-      <MyButtonComponent nameClassCss={'btn'}>Button 1</MyButtonComponent>
-      <MyButtonComponent nameClassCss={'btn'}>Button 2</MyButtonComponent>
       </>
   )
 }
