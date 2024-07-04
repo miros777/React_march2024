30de2b88ecb5d6b29991449122c297adc92ca5f1
miros777 <syniavskyi1984@gmail.com>
2024-07-04 14:40:03 +0300

    change and finish hooks useToggle, usePrevious

--- src/hooks/useToggle.tsx
+++ src/hooks/useToggle.tsx
@@ -1,15 +1,8 @@
-import React, {useEffect, useState} from 'react';
+import { useState} from 'react';
 
 export const useToggle = (init:boolean) => {
     const [toggle, setToggle] = useState<boolean>(init);
-    const change = () => setToggle((prevState):boolean => {
-        if (prevState === true) {
-            prevState = false;
-        } else {
-            prevState = true;
-        }
-        return prevState;
-    });
+    const change = () => { setToggle(prevState => !prevState) }
 
     return {toggle, change};
 }
