e1682005abaaaf1670ba91339ae702e40e93f8ef
miros777 <syniavskyi1984@gmail.com>
2024-08-04 23:04:32 +0300

    finish Zustant task

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,32 @@
-import React, {createContext, useContext, useState} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import MainComponent from "./components/MainComponent";
 import DecrementComponent from "./components/DecrementComponent";
 import IncrementComponent from "./components/IncrementComponent";
+import {create} from "zustand";
 
 type storeType = {
     counter: number,
     inc: () => void,
     dec: () => void
 }
-
-const MyContext = createContext<storeType>({
+export const useStore = create<storeType>((set) => ({
     counter: 0,
     inc: () => {
+        set((state) => ({counter: state.counter + 1}));
     },
     dec: () => {
+        set((state) => ({counter: state.counter - 1}));
     }
-});
-export const useAppContext = ():storeType => useContext(MyContext);
+}));
 
 const App = () => {
 
-    const [globalCounter, setGlobalCounter] = useState<number>(0)
-
     return (
         <div>
-            <MyContext.Provider value={{
-                counter: globalCounter,
-                inc: () => {
-                        setGlobalCounter(prevState => ++prevState)
-                },
-                dec: () => {
-                    setGlobalCounter(prevState => --prevState)
-                }
-            }}>
-                <MainComponent/>
-                <DecrementComponent/>
-                <IncrementComponent/>
-            </MyContext.Provider>
+            <MainComponent/>
+            <DecrementComponent/>
+            <IncrementComponent/>
         </div>
     )
 }
--- src/components/IncrementComponent.tsx
+++ src/components/IncrementComponent.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import {useAppContext} from "../App";
+import {useStore} from "../App";
 
 const IncrementComponent = () => {
-    let counter = useAppContext();
+    let counter = useStore();
 
     return (
         <div>
