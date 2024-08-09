05daec1bb9d95e477051f9f9d5cf9fa61752727d
miros777 <syniavskyi1984@gmail.com>
2024-08-09 09:23:40 +0300

    add useStorePost but not finished

--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import {useStoreCounter} from "../store/useStoreCounter";
+import {useStorePosts} from "../store/useStorePosts";
 
 const MainComponent = () => {
     let counter = useStoreCounter();
+    let posts = useStorePosts();
+
+    console.log(posts.getAllPosts());
+    console.log(posts.getAllPosts());
 
     return (
         <div>
--- src/store/useStorePosts.tsx
+++ src/store/useStorePosts.tsx
@@ -2,9 +2,17 @@ import {create} from "zustand";
 import {IPost} from "../models/IPost";
 
 type storeType = {
-    post: IPost[]
+    post: IPost[],
+    getAllPosts: () => Promise<IPost[]>
 }
-export const useStorePosts = create<storeType>((set)=> ({
-    post: []
+export const useStorePosts = create<storeType>((set) => ({
+    post: [],
+    getAllPosts: () => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(data => {
+                set((state) => ({post: data.data}));
+            })
+    }
 }));
 
